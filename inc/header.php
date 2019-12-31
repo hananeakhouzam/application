739844cd0002d6a50cc96755ec33efab9a18eedc
takiddine <takiddine.job@gmail.com>
2019-12-31 15:10:06 +0100

    first commit

---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
+	<!-- core:css -->
+	<link rel="stylesheet" href="/assets/vendors/core/core.css">
+	<!-- endinject -->
+  <!-- plugin css for this page -->
+	<link rel="stylesheet" href="/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+	<!-- end plugin css for this page -->
+	<!-- inject:css -->
+	<link rel="stylesheet" href="/assets/fonts/feather-font/css/iconfont.css">
+	<!-- endinject -->
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="/assets/css/demo_1/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="/assets/images/favicon.png" />
+  <!-- Global site tag (gtag.js) - Google Analytics start -->
+</head>
+
+
+
+
+
+<body class="sidebar-dark">
+	<div class="main-wrapper">
+
+	
+    <?php require_once 'sidebar.php'; ?>
+
+    
---
+++ inc/sidebar.php
@@ -0,0 +1,151 @@
+	<!-- partial:partials/_sidebar.html -->
+		<nav class="sidebar">
+      <div class="sidebar-header">
+        <a href="#" class="sidebar-brand">
+          Noble<span>UI</span>
+        </a>
+        <div class="sidebar-toggler not-active">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div class="sidebar-body">
+        <ul class="nav">
+       
+           <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#icons" role="button" aria-expanded="false" aria-controls="icons">
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Users</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="icons">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="" class="nav-link">All users</a>
+                </li>
+                <li class="nav-item">
+                  <a href="" class="nav-link">Costumers</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+         
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Phones</span>
+            </a>
+          </li>
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Contacts</span>
+            </a>
+          </li>
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Calls History</span>
+            </a>
+          </li>
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">SMS</span>
+            </a>
+          </li>
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Settings</span>
+            </a>
+          </li>
+         
+          
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Gallery</span>
+            </a>
+          </li>
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Payments History</span>
+            </a>
+          </li>
+         
+         
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Browser History</span>
+            </a>
+          </li>
+         
+
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Apps Installed</span>
+            </a>
+          </li>
+         
+            <li class="nav-item">
+            <a class="nav-link"  href="#icons" role="button" >
+              <i class="link-icon" data-feather="smile"></i>
+              <span class="link-title">Media</span>
+            </a>
+          </li>
+         
+        </ul>
+      </div>
+    </nav>
+    
+        
+    <nav class="settings-sidebar">
+      <div class="sidebar-body">
+        <a href="#" class="settings-sidebar-toggler">
+          <i data-feather="settings"></i>
+        </a>
+        <h6 class="text-muted">Sidebar:</h6>
+        <div class="form-group border-bottom">
+          <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarLight" value="sidebar-light" checked>
+              Light
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarDark" value="sidebar-dark">
+              Dark
+            </label>
+          </div>
+        </div>
+        <div class="theme-wrapper">
+          <h6 class="text-muted mb-2">Light Theme:</h6>
+          <a class="theme-item active" href="../demo_1/dashboard-one.html">
+            <img src="/assets/images/screenshots/light.jpg" alt="light theme">
+          </a>
+          <h6 class="text-muted mb-2">Dark Theme:</h6>
+          <a class="theme-item" href="../demo_2/dashboard-one.html">
+            <img src="/assets/images/screenshots/dark.jpg" alt="light theme">
+          </a>
+        </div>
+      </div>
+    </nav>
+		<!-- partial -->
